8a691ff705e0bec67fa076b19c7a3a83dfbf3519
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-12-10 21:44:49 +0530

    beryllium: Include MiuiCam and BCR

--- device.mk
+++ device.mk
@@ -11,9 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
+# BCR
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
 TARGET_SCREEN_WIDTH := 1080
+
+# Camera
+$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
 # FM
 PRODUCT_PACKAGES += \
