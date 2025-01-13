8bff2c09821cb93e390c9db1d3bfc956b25a9f0e
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-01-13 20:15:38 +0530

    beryllium: add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,32 @@
+# Common Device Tree
+rm -rf device/xiaomi/sdm845-common
+git clone https://github.com/PocoF1-Resources/android_device_xiaomi_sdm845-common.git -b 15.0 device/xiaomi/sdm845-common --depth=1
+
+# Vendor Tree
+rm -rf vendor/xiaomi/beryllium
+git clone https://github.com/PocoF1-Resources/proprietary_vendor_xiaomi_beryllium.git -b 15.0 vendor/xiaomi/beryllium --depth=1
+
+# Common Vendor Tree
+rm -rf vendor/xiaomi/sdm845-common
+git clone https://github.com/PocoF1-Resources/proprietary_vendor_xiaomi_sdm845-common.git -b 15.0 vendor/xiaomi/sdm845-common --depth=1
+
+# Kernel Tree
+rm -rf kernel/xiaomi/sdm845
+git clone https://github.com/PocoF1-Resources/android_kernel_xiaomi_sdm845.git -b 15.0 kernel/xiaomi/sdm845 --depth=1
+
+# MiCam
+rm -rf vendor/miuicamera
+git clone https://github.com/Legendleo90/vendor_miuicamera.git -b fourteen vendor/miuicamera --depth=1
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.1 hardware/xiaomi
+
+# Viper4FX
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+
+# BCR
+rm -rf vendor/bcr
+git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr --depth=1
+
