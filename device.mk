75110bde95d20b90d336b216d3786d745f2e6dfb
Bruno Martins <bgcngm@gmail.com>
2025-02-10 11:39:41 +0000

    beryllium: Remove implicit FM radio app deps
    Change-Id: I96407b03f9039433c3ccf1dc269952510304f0ed

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2024 The LineageOS Project
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,9 +20,7 @@ $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+    FM2
 
 $(call soong_config_set,libfmjni,vendor,qcom)
 
