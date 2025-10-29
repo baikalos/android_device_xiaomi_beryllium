cb7ff50753a82ba33e0bc1243da693aa72c7ec5c
Akshat <Legendleo90@users.noreply.github.com>
2025-10-29 11:34:53 +0530

    beryllium: Bringup @AxionAOSP

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,6 +9,23 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Axion Flags
+AXION_MAINTAINER := Akshat
+AXION_PROCESSOR := Snapdragon_845
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := true
+
+# GPU Flags
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/min_clock_mhz
+
+# Camera Flags
+AXION_CAMERA_REAR_INFO := 12,5
+AXION_CAMERA_FRONT_INFO := 20
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
